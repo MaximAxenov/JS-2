79c94b3296cf002410c943ccbb1f49c6cf5e5df7
МаксимАксёнов <aksenovmax777@gmail.com>
2021-07-05 13:32:05 +0300

    lesson-5
    lesson-5

---
+++ js-2.5/script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+let app = new Vue ({
+    el: '#app',
+    data: {
+        catalogUrl: '/catalogData.json',
+        products: [],
+        imgCatalog: 'http://placehold.it/250x150',
+        searchText: '',
+        filteredProducts: []
+    },
+    methods: {
+        getJson (url) {
+            return fetch(url)
+                .then (result => result.json())
+                .catch (error => {
+                    console.log (error)
+                })
+        },
+        addProduct (product) {
+            console.log (product.id_product);
+        },
+
+        searchFor () {
+            let text = this.searchText.toLowerCase().trim();
+            
+            if (text === '') {
+                this.filteredProducts = this.products;
+            } else {
+                this.filteredProducts = this.products.filter((el) => {
+                    return el.product_name.toLowerCase().includes(text);
+                });
+            }
+        }
+    },
+    mounted () {
+        this.getJson (`${API + this.catalogUrl}`)
+            .then (data => {
+                for (let el of data) {
+                    this.products.push (el)
+                }
+            });
+        this.filteredProducts = this.products;
+    }
+});
+
+
